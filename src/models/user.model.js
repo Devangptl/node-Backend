c057e7178ffacf61b4572514c01ddbf223d6a00d
Devangptl <devangmpatel51@gmail.com>
2023-12-22 14:05:57 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,9 +60,9 @@ const userSchema = new mongoose.Schema({
 {timestamps : true})
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next()
+    if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password , 10)
+    this.password = await bcrypt.hash(this.password , 10)
     next()
 } )
 
